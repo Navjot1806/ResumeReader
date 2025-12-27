44ea6410e3c68055449d95b6d544ff562f0730ec
Navjyot <navjyotmultani18@gmail.com>
2025-12-26 17:23:27 -0800

    Initial Commit

---
+++ ReadAITests/ReadAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ReadAITests.swift
+//  ReadAITests
+//
+//  Created by Navjyotsingh Multani on 12/26/25.
+//
+
+import Testing
+@testable import ReadAI
+
+struct ReadAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
